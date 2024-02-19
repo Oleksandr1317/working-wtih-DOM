e75201c751ce593125e1472c9e2a513e6a984769
general <sgladkoskoka@gmail.com>
2024-02-19 21:59:08 +0200

    Solve practice with DOM

---
+++ app.js
@@ -0,0 +1,7 @@
+// document.body.children[1].children[0].href = 'https://google.com'
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('a');
+anchorElement.href = 'https://bing.com';
---
+++ exercise.js
@@ -0,0 +1,19 @@
+// task 1
+let h1E = document.body.children[0];
+console.dir(h1E);
+
+// task 2
+console.dir(h1E.parentElement);
+
+console.dir(h1E.nextElementSibling);
+
+// task 3
+let h1Element = document.getElementById('main-text');
+console.dir(h1Element);
+
+//task 4
+let paragraph = document.querySelector('.second-paragraph');
+console.dir(paragraph);
+
+//task 5
+paragraph.textContent = 'Test';
